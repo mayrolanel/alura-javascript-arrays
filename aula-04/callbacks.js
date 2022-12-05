ca4ba4916b944ae8141447608b12408e7e8b58d5
Mayro Lanel da Silva Belizario <mayro.belizario@GTC-N-795.trinuscapital.com>
2022-12-05 20:20:02 -0300

    projeto final do curso de array cm javascript

---
+++ aula-04/callbacks.js
@@ -0,0 +1,8 @@
+const nomes = ["Evaldo", "Mari", "Camis"];
+
+function imprimeNome(nome) {
+    console.log(nome)
+}
+
+nomes.forEach( imprimeNome )
+
---
+++ aula-05/copiando-arrays.js
@@ -0,0 +1,8 @@
+const notas = [7, 7, 8, 9];
+
+const novaNotas = [...notas];
+
+novaNotas.push(10);
+
+console.log(`Novas notas: ${novaNotas}`)
+console.log(`Notas originais: ${notas}`)
